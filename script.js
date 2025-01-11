24beadbe653ca97622cbfdb87e4420c2faa41593
raniaayuni <raniaaayunii@gmail.com>
2025-01-11 18:07:22 +0700

    update

--- script.js
+++ script.js
@@ -13,14 +13,6 @@ closeBtn.addEventListener('click', () => {
     sideMenu.style.display = "none";
 });
 
-// Theme toggler
-const themeToggler = document.querySelector('.theme-toggler');
-themeToggler.addEventListener('click', () => {
-    document.body.classList.toggle('dark-theme-variables');
-    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
-    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
-});
-
 // Fungsi untuk memuat konten berdasarkan klik
 function loadContent(target) {
     switch (target) {
